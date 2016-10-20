1cb3191cb82520d1b54c227cea0eb3fe1f4b6c93
chemzqm <chemzqm@gmail.com>
2016-10-20 17:47:48 +0800

    route to current page on refresh

--- src/util.js
+++ src/util.js
@@ -13,7 +13,7 @@ export function createFrame(id, src, hidden, parent = document.body) {
   el.setAttribute('sandbox', "allow-scripts allow-same-origin allow-forms allow-modals")
   el.setAttribute('frameborder', "0")
   el.setAttribute('width', hidden ? "0": "100%")
-  el.setAttribute('height', hidden ? "0": `${document.body.clientHeight - 42}`)
+  el.setAttribute('height', hidden ? "0": "100%")
   if (hidden) {
     el.setAttribute('style', 'width:0;height:0;border:0; display:none;')
   }
@@ -27,6 +27,12 @@ export function parsePath(path) {
     path: parts[0],
     query: qs.parse(parts[1])
   }
+}
+
+export function validPath(p) {
+  let pages = window.__wxConfig__.pages
+  let {path} = parsePath(p)
+  return pages.indexOf(path) !== -1
 }
 
 export function isTabbar(url) {
@@ -45,6 +51,29 @@ export function reload() {
 export function redirectTo(url) {
   let home = `${location.protocol}//${location.host}`
   if (typeof history.replaceState == 'function') {
-    history.replaceState({}, '', `${home}?!${url}`)
+    history.replaceState({}, '', `${home}#!${url}`)
   }
 }
+
+export function getRedirectData(url, webviewID) {
+  return {
+    to: 'backgroundjs',
+    msg: {
+      eventName: 'INVOKE_METHOD',
+      data: {
+        data: {
+          name: 'navigateTo',
+          args: {
+            url: url
+          }
+        },
+        options: {
+          timestamp: Date.now()
+        }
+      }
+    },
+    comefrom: 'webframe',
+    webviewID: webviewID
+  }
+}
+
