cb0c564d88fab234a7f66d315f470a3b617feabb
chemzqm <chemzqm@gmail.com>
2016-10-23 00:49:16 +0800

    show panels only for wept pages

--- extensions/devtools.js
+++ extensions/devtools.js
@@ -1,21 +1,28 @@
 /*global chrome*/
 "use strict"
 
-
-// 添加appdata pannel
-chrome.devtools.panels.create("AppData",
-  "",
-  "appdata/index.html",
-  function(panel) {
-    //console.log(panel)
+chrome.devtools.inspectedWindow.eval(`window.__wxConfig__`, (res, isErr) => {
+  if (isErr) return
+  if (res.appid && res.directory) {
+    init()
   }
-)
+})
 
-chrome.devtools.panels.create("Storage",
-  "",
-  "storage/index.html",
-  function(panel) {
-    //console.log(panel)
-  }
-)
+function init() {
+  // 添加appdata pannel
+  chrome.devtools.panels.create("AppData",
+    "",
+    "appdata/index.html",
+    function(panel) {
+      //console.log(panel)
+    }
+  )
 
+  chrome.devtools.panels.create("Storage",
+    "",
+    "storage/index.html",
+    function(panel) {
+      //console.log(panel)
+    }
+  )
+}
