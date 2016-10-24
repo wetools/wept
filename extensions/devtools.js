ec548d5d1ef3c8b08288722646bc0c15fdc8253b
chemzqm <chemzqm@gmail.com>
2016-10-25 06:13:32 +0800

    improve load of devtools panel

--- extensions/devtools.js
+++ extensions/devtools.js
@@ -1,30 +1,52 @@
 /*global chrome*/
 "use strict"
 
-setTimeout(function () {
-  chrome.devtools.inspectedWindow.eval(`window.__wxConfig__`, (res, isErr) => {
-    if (isErr) return
-    if (res.appid && res.directory) {
-      init()
-    }
+var panelCreated = false
+
+function createPanelIfWeappLoaded() {
+  if (panelCreated) return
+  checkWeapp(function (isWeapp) {
+    if (!isWeapp || panelCreated) return
+    panelCreated = true
+    clearInterval(loadCheckInterval)
+
+    // 添加appdata pannel
+    chrome.devtools.panels.create("AppData",
+      "",
+      "appdata/index.html",
+      function(panel) {
+        //console.log(panel)
+      }
+    )
+
+    chrome.devtools.panels.create("Storage",
+      "",
+      "storage/index.html",
+      function(panel) {
+        //console.log(panel)
+      }
+    )
   })
+}
+
+
+function checkWeapp(cb) {
+  chrome.devtools.inspectedWindow.eval(`window.__wxConfig__`, (res, isErr) => {
+    if (res && res.appid && res.directory) {
+      return cb(true)
+    }
+    cb(false)
+  })
+}
+
+chrome.devtools.network.onNavigated.addListener(function() {
+  setTimeout(function () {
+    createPanelIfWeappLoaded()
+  }, 1000)
+})
+
+var loadCheckInterval = setInterval(function() {
+  createPanelIfWeappLoaded()
 }, 1000)
 
-function init() {
-  // 添加appdata pannel
-  chrome.devtools.panels.create("AppData",
-    "",
-    "appdata/index.html",
-    function(panel) {
-      //console.log(panel)
-    }
-  )
-
-  chrome.devtools.panels.create("Storage",
-    "",
-    "storage/index.html",
-    function(panel) {
-      //console.log(panel)
-    }
-  )
-}
+createPanelIfWeappLoaded()
